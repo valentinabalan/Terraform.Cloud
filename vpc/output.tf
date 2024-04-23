47e5b0d5b0d294df89bfcc82ca08eebd54356d76
valentinabalan <159856621+valentinabalan@users.noreply.github.com>
2024-04-22 19:57:18 -0500

    added outputs

---
+++ vpc/output.tf
@@ -0,0 +1,11 @@
+ output vpc_id {
+    value = module.vpc_id
+ }
+
+ output private_subnets {
+    value = module.private_subnets
+ }
+
+  output public_subnets {
+    value = module.public_subnets
+ }
